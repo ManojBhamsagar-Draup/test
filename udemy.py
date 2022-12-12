7df485f00d16852d6a262aa40ee57c0e91c37656
manoj-b-b <manojb.bhamsagar@draup.com>
2022-12-12 19:29:37 +0530

    modified udemy

--- udemy.py
+++ udemy.py
@@ -15,11 +15,28 @@ class UdemyDetailsPostProcessor(IPostProcessor):
         course_data: list = output.data
         self.extracted_data.data = self.process_udemy_details_data(course_data)
 
-    @staticmethod
-    def process_udemy_details_data(course_data):
+    def process_udemy_details_data(self, course_data):
         target_data = course_data[0].get('details', {}).get('requirements', "")
         target_data = json.loads(target_data)
         scalped = scalpl.Cut(target_data)
-        requirements = scalped.get('serverSideProps').get('course').get('prerequisites')
+        requirements = scalped.get('serverSideProps.course.prerequisites')
+        other_details = self.get_other_details(target_data)
         course_data[0]['details']['requirements'] = requirements
+        course_data[0]['details']['other_details'] = other_details
         return course_data
+
+    @staticmethod
+    def get_other_details(self, other_data):
+        other_details = []
+        data = scalpl.Cut(other_data)
+        instruction_level = data.get('serverSideProps.course.instructionalLevel')
+        instructors = data.get('serverSideProps.course.instructors.instructors_info')
+        number_of_reviews = data.get('serverSideProps.course.numReviews')
+        number_of_students = data.get('serverSideProps.course.numStudents')
+        objectives = data.get('serverSideProps.course.objectives')
+        rating = data.get('serverSideProps.course.rating')
+        target_audiences = data.get('serverSideProps.course.targetAudiences')
+        other_details.append({'instruction_level': instruction_level, 'instructors': instructors,
+                              'number_of_reviews': number_of_reviews, 'number_of_students':number_of_students,
+                              'objectives': objectives, 'rating': rating, 'target_audiences': target_audiences})
+        return other_details
